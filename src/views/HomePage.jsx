699e29644919e9d3f344f0bfc11653a39edaa556
agatapopek <eaa24agpo@students.eaaa.dk>
2025-02-13 14:30:23 +0100

    yellow

--- src/views/HomePage.jsx
+++ src/views/HomePage.jsx
@@ -1,16 +1,22 @@
+import { Link } from 'react-router-dom';
 import cookImage from '../assets/images/cook.webp';
+import pizzaImage from '../assets/images/pizza.jpg';
 
 export default function HomePage(){
     return(
         <>
         <div className='hero-section'>
             <img src={cookImage}/>
-        <h1>welcome to DishDelights</h1>
+        <h1>WELCOME TO DISHDELIGHTS</h1>
         <h3>Be the real cook and create your own recipes</h3>
-        <button>Your Favourites</button>
+        <button><Link to="favourites">Your Favourites</Link></button>
         </div>
         <section>
-            
+            <h1 className='bestseller-text'>BESTSELLERS</h1>
+            <img className="bestsellers-img" src={pizzaImage}/>
+            <img className="bestsellers-img" src={pizzaImage}/>
+            <img className="bestsellers-img" src={pizzaImage}/>
+            <img className="bestsellers-img" src={pizzaImage}/>
         </section>
         </>
     )
